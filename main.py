a043aa9750a94e9b9223e6d0d9379bb238b7847c
BigBrainTime <57951435+BigBrainTime@users.noreply.github.com>
2023-06-08 18:27:29 -0500

    downloader finished

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
-import requests
+import requests, os
 
-#mods = requests.get()
+debug = False
+
+if debug:
+    with open('mods.txt', 'r') as file:
+        mods = file.read()
+
+else:
+    mods = requests.get('https://github.com/BigBrainTime/mc-mods-downloader/raw/main/mods.txt').content
+    mods = mods.decode()
+
+mods = mods.split('\n')
+
+if 'mods' not in os.listdir():
+    os.mkdir('mods')
+    installed = []
+else:
+    installed = os.listdir('mods')
+
+for mod in mods:
+    name = mod.split('/')[6]
+    if name not in installed:
+        data = requests.get(mod).content
+
+        with open(f'mods/{name}', 'wb') as file:
+            file.write(data)
+
+        print(f'{name} installed')
+
+    else:
+        print(f'{name} already installed')
