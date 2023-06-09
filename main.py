ee39ad61ba298a0882b675d71bbc16e7e29b6886
BigBrainTime <57951435+BigBrainTime@users.noreply.github.com>
2023-06-08 19:02:29 -0500

    exe packaged

--- main.py
+++ main.py
@@ -18,6 +18,17 @@ if 'mods' not in os.listdir():
 else:
     installed = os.listdir('mods')
 
+if 'forgeinstaller.jar' not in os.listdir():
+    data = requests.get('https://maven.minecraftforge.net/net/minecraftforge/forge/1.19.4-45.1.0/forge-1.19.4-45.1.0-installer.jar').content
+
+    with open(f'forgeinstaller.jar', 'wb') as file:
+        file.write(data)
+
+    print('forge installed')
+
+else:
+    print('forge detected')
+
 for mod in mods:
     name = mod.split('/')[6]
     if name not in installed:
@@ -29,4 +40,4 @@ for mod in mods:
         print(f'{name} installed')
 
     else:
-        print(f'{name} already installed')
+        print(f'{name} detected')
